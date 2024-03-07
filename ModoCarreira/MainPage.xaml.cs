6467a5c543537ffe5a50ed96ff42c6c36893677a
GuiMatheus <gui.igarashi@hotmail.com>
2024-03-07 07:57:19 -0300

    Sexto Commit

--- ModoCarreira/MainPage.xaml.cs
+++ ModoCarreira/MainPage.xaml.cs
@@ -15,9 +15,27 @@ public partial class MainPage : ContentPage
 		Application.Current.MainPage = new MCPage();
 		}
 		
-	
+	private void OpenTutorial(object sender, EventArgs args)
+			{
+			frameTutorial.IsVisible = true;
+			}
 
+	private void BackButton(object sender, EventArgs args)
 
+  	 	 {
+		frameTutorial.IsVisible = false;
+	 	 }
+
+	private void OpenAbout(object sender, EventArgs args)
+		{
+			frameAbout.IsVisible = true;
+		}
+
+	private void BackAboutButton(object sender, EventArgs args)
+
+  	 	 {
+		frameAbout.IsVisible = false;
+	 	 }
 
 
 
